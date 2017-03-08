fa428f6a196cae77839bcec88ab8bd6211898d61
chepundup <mazen.elbawab@gmail.com>
2017-03-08 10:51:46 -0500

    Adding Common and Core projects to the solution

---
+++ HDK_COMMON/HDK.Utils/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HDK.Utils
+{
+    public class Class1
+    {
+    }
+}
---
+++ HDK_CORE/HDK.Comm/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HDK.Comm
+{
+    public class Class1
+    {
+    }
+}
